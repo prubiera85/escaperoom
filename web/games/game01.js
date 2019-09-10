775638b76db64d8daf050ef4a21d0f0232af1ccc
Paula Rubiera <45872020+prubiera85@users.noreply.github.com>
2019-09-10 17:50:43 +0200

    Add files via upload

---
+++ web/games/game01.js
@@ -0,0 +1,21 @@
+var game01 = {
+    "title": "Máquina 1: WGIBSON",
+    "description": "Atrapa al grupo de hackers que intenta liberar la IA llamada Neuromante. A lo largo de la máquina virtual se podrán localizar diferentes servicios online funcionando en la máquina, donde se han ocultado varias banderas, y en el disco duro de la misma, tras haber obtenido una sesión remota más banderas. Solo mediante el uso de técnicas de pentesting se podrán obtener todas las banderas que hay disponibles en la máquina WGIBSON.",
+    "cards":[
+        {
+            "id": 0,
+            "story": "<p>Alguien está intentando liberar Neuromante... </p><p>¡Averigua de dónde procede este ataque y atrapa al culpable antes de que se te adelanten y cobren la recompensa!</p>",
+            "instructions": "<p>Instala la máquina virtual proporcionada y utiliza técnicas de pentesting para obtener las diferentes banderas que te permitirán avanzar en la historia y resolver la escape room.</p>",
+            "links": [
+                {"txt": "Descargar máquina", "href": "resources/machine.zip"},
+                {"txt": "Instrucciones de instalación", "href": "resources/instrucciones.pdf"}
+            ]
+        },
+        {
+            "id": 1,
+            "story": "<p>¿A qué estás esperando?</p><p>Analiza ahora mismo las vulnerabilidades en los servicios para ver de dónde procede este ataque. ¡No podemos permitirnos que Neuromante sea liberada!</p>",
+            "instructions": "<p>Introduce la clave obtenida en la bandera 1 para continuar la historia</p>",
+            "access_code": "ABC"
+        }
+    ]
+}
--- web/js/core.js
+++ web/js/core.js
@@ -3,51 +3,139 @@
     Oneclick 2019
  *******************************************************************/
 
-// var scope_path = "sources/";    // ruta de libro por defecto
-var manifiesto = "/imsmanifest.xml";
-var selectedGame ;
+var selectedGame = getURLParameter("game");
+var gameData;
 
 (function(){
-    
-
 
     // Deeplinking con routie
 
     routie({
         '': function() {
-            hideAllModules();
+            //hideAllCards();
         }, 
 
         'start': function() {
-            hideAllModules();
-            initScreen("start");
+           
         }, 
         
         'home': function() {
-            hideAllModules();
-            initScreen("home");
+   
         },
 
 
         'topic/:topic': function(topic) {
-            hideAllModules();
-            initScreen("topic",topic);
+
         },
 
         'listening/:topic': function(topic) {
-            hideAllModules();
-            initScreen("listening",topic);
-        }
 
-        
+        }
     });
 
-
+    loadGameData(selectedGame);
+    renderGame();
       
  
-
-
 }());
+
+
+function loadGameData(selectedGame){
+    switch(selectedGame){
+        case 1:
+            gameData = game01;
+        break;
+        case 2:
+            gameData = game02;
+        break;
+        default:
+            gameData = game01;
+        break;
+    }
+}
+
+function renderGame(){
+    
+    console.log(gameData.title);
+    console.log(gameData.description);
+
+    $(".game_title").html(gameData.title);
+
+    renderCard(0);
+    renderCard(1);
+
+}
+
+
+
+function renderCard(card_number){
+
+    var thisCard = gameData.cards[card_number];
+
+    // Creamos los botones de enlaces si existen 
+
+    var linksHtml = "";  
+
+    if (thisCard.links){
+        thisCard.links.forEach (function(link) {
+            linksHtml += `<a href="${link.href}" class="corner_box btn_link">
+            <div class="corner_line corner_topleft"></div>
+            <div class="corner_line corner_topright"></div>
+            <div>${link.txt}</div>
+            <div class="corner_line corner_bottomleft"></div>
+            <div class="corner_line corner_bottomright"></div>
+        </a>`;
+        });
+    }
+
+    // Creamos el candado si existe
+
+    var accessHtml = "";
+
+    if (thisCard.access_code){
+        accessHtml += `<section class="code_prompt">
+            <p class="warning">WARNING</p>
+            <p class="text">Introduce el código de acceso para continuar</p>
+            <input class="access_code_input" type="text" name="access_code">
+            <div class="corner_box btn_validate">
+                <div class="corner_line corner_topleft"></div>
+                <div class="corner_line corner_topright"></div>
+                <div>Validar código</div>
+                <div class="corner_line corner_bottomleft"></div>
+                <div class="corner_line corner_bottomright"></div>
+            </div>
+        </section>`;
+    }
+
+    // Creamos la tarjeta de juego
+
+    var cardHtml = "";
+
+    cardHtml += `<section class="game_card" id="game_card_${thisCard.id}">
+    <section class="story">${thisCard.story}</section>
+    <section class="instructions">${thisCard.instructions}</section>`;
+    cardHtml += `${accessHtml}`;        
+    cardHtml += `<section class="navigation_bar">          
+        ${linksHtml}
+        <a href="#" class="corner_box btn_continue">
+            <div class="corner_line corner_topleft"></div>
+            <div class="corner_line corner_topright"></div>
+            <div>Continuar</div>
+            <div class="corner_line corner_bottomleft"></div>
+            <div class="corner_line corner_bottomright"></div>
+        </a>
+    </section>`
+    ;
+
+    cardHtml += `</section>`;
+
+    // Añadimos la tarjeta de juego
+
+    $(".game_container").append(cardHtml);
+}
+
+
+
 
 
 
@@ -69,8 +157,12 @@ $(document).on("click", ".btn_topic3", function(){
 /*******************************************************************/
 
 
-function hideAllModules(){
-    $(".module").hide();
+function hideAllCards(){
+    $(".game_card").hide();
+}
+
+function showCard(card_number){
+    $("#game_card_" + card_number).show();
 }
 
 
